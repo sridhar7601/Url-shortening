9b3cddd5f55f049845a792aa37b62b27ce834498
srisru <sridzar1234@gmail.com>
2023-06-11 11:32:45 +0530

    updated the code

--- app.js
+++ app.js
@@ -1,56 +1,53 @@
 const express = require('express');
-const shortid = require('shortid');
-const copyPaste = require('copy-paste');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
-// Create an object to store the URLs
-const urlDatabase = {};
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// Home route
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-// Shorten URL route
-app.post('/shorten', (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = shortid.generate();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
-  // Store the shortened URL in the database
-  urlDatabase[shortURL] = longURL;
-
-  // Return the shortened URL
-  res.send({ shortURL: `${req.hostname}/${shortURL}` });
-});
-
-// Redirect route
-app.get('/:shortURL', (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-
-  if (longURL) {
-    res.redirect(longURL);
-  } else {
-    res.status(404).send('URL not found');
+app.get('/', async (req, res) => {
+  try {
+    const shortUrls = await ShortUrl.find();
+    res.render('index', { shortUrls: shortUrls });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+app.post('/shortUrls', async (req, res) => {
+  try {
+    await ShortUrl.create({ full: req.body.fullUrl });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Copy button functionality
-const copyToClipboard = (text) => {
-  copyPaste.copy(text, () => {
-    console.log('Copied to clipboard:', text);
-  });
-};
+app.get('/:shortUrl', async (req, res) => {
+  try {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (!shortUrl) {
+      return res.sendStatus(404);
+    }
 
-app.post('/copy', (req, res) => {
-  const shortURL = req.body.shortURL;
-  copyToClipboard(shortURL);
-  res.send({ message: 'URL copied to clipboard' });
+    shortUrl.clicks++;
+    await shortUrl.save();
+
+    res.redirect(shortUrl.full);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(process.env.PORT || 5002, () => {
+  console.log('App listening on port 5002!');
 });
